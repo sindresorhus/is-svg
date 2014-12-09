9b652f7967b5c4eb03cfc6d9a992b20dacf4b664
Sindre Sorhus <sindresorhus@gmail.com>
2014-12-09 12:14:21 +0700

    loosen up the check - fixes imagemin/imagemin-svgo#6

--- test.js
+++ test.js
@@ -5,4 +5,5 @@ var isSvg = require('./');
 
 it('should detect SVG from Buffer', function () {
 	assert(isSvg(fs.readFileSync('fixture.svg')));
+	assert(isSvg('<svg width="100" height="100" viewBox="0 0 30 30" version="1.1"></svg>'));
 });
