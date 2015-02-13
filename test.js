6cb6a6bff04d5a20791f1e49b7c39908ea1a2a28
Simon Coffey <simon@urbanautomaton.com>
2015-02-13 09:27:12 +0000

    Tighten detection regexp to reject more non-svg input
    The existing regexp matched any string with an occurrence of an <svg> tag
    anywhere in the document.
    
    By looking for a pair of tags and anchoring to the beginning and end of the
    buffer, we can avoid a lot of false positives, including the README of this
    module and the module's own source code.

--- test.js
+++ test.js
@@ -8,3 +8,19 @@ it('should detect SVG from Buffer', function () {
 	assert(isSvg('<svg width="100" height="100" viewBox="0 0 30 30" version="1.1"></svg>'));
 	assert(!isSvg(fs.readFileSync('fixture.jpg')));
 });
+
+it('should not match svg tags in the middle of a document', function() {
+  assert(!isSvg('this is not svg, but it mentions <svg> tags'));
+});
+
+it('should not match malformed input', function() {
+  assert(!isSvg('<svg> hello I am an svg oops maybe not'));
+});
+
+it('should not think the readme is an SVG', function() {
+  assert(!isSvg(fs.readFileSync('readme.md')));
+});
+
+it('should not think its own code is an SVG', function() {
+  assert(!isSvg(fs.readFileSync('index.js')));
+});
