9a76bd8957f0b00e9db46e046d0f74be059f4620
Sindre Sorhus <sindresorhus@gmail.com>
2017-08-15 00:18:08 +0200

    Add test for non-english characters
    Closes #10

--- test.js
+++ test.js
@@ -26,3 +26,19 @@ test('invalid SVGs', t => {
 	t.false(m(fs.readFileSync('readme.md')));
 	t.false(m(fs.readFileSync('index.js')));
 });
+
+test('supports non-english characters', t => {
+	t.true(m(`<svg xmlns="http://www.w3.org/2000/svg"
+	     width="100%" height="100%" viewBox="0 0 400 400"
+	     direction="rtl" xml:lang="fa">
+
+	  <title direction="ltr" xml:lang="en">Right-to-left Text</title>
+	  <desc direction="ltr" xml:lang="en">
+	    A simple example for using the 'direction' property in documents
+	    that predominantly use right-to-left languages.
+	  </desc>
+
+	  <text x="200" y="200" font-size="20">داستان SVG 1.1 SE طولا ني است.</text>
+
+	</svg>`));
+});
