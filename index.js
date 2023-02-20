7ec2574e922054f0792125a4fa09cfc990babc4f
gartcimore <gart.algar@gmail.com>
2023-02-20 13:41:45 +0100

    upgrade to fast-xml-parser 4.1.3

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 'use strict';
-const parser = require('fast-xml-parser');
+const {XMLParser, XMLValidator} = require('fast-xml-parser');
 
 const isSvg = input => {
 	if (input === undefined || input === null) {
 		return false;
 	}
 
-	// TODO: Remove the `.replace` call when using `fast-xml-parser@4` which has fixed the bug.
-	input = input.toString().trim().replace(/\n/g, ' ');
+	input = input.toString().trim();
 
 	if (input.length === 0) {
 		return false;
 	}
 
 	// Has to be `!==` as it can also return an object with error info.
-	if (parser.validate(input) !== true) {
+	if (XMLValidator.validate(input) !== true) {
 		return false;
 	}
 
 	let jsonObject;
+	const parser = new XMLParser();
+
 	try {
 		jsonObject = parser.parse(input);
 	} catch (_) {
