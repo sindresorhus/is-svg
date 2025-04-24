027beee712fdf9f29c3cad761f6857aa38951fcf
Borewit <Borewit@users.noreply.github.com>
2025-04-24 14:11:59 +0200

    Add support for `Uint8Array`

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-import {XmlTextDetector} from '@file-type/xml';
+import {XmlTextDetector, isXml} from '@file-type/xml';
 
 export default function isSvg(string) {
-	if (typeof string !== 'string') {
-		throw new TypeError(`Expected a \`string\`, got \`${typeof string}\``);
-	}
+	if (typeof string === 'string') {
+		string = string.trim();
 
-	string = string.trim();
+		if (string.length === 0) {
+			return false;
+		}
 
-	if (string.length === 0) {
+		const xmlTextDetector = new XmlTextDetector();
+		xmlTextDetector.write(string);
+		return xmlTextDetector.isValid() && xmlTextDetector.fileType?.ext === 'svg';	}
+
+	if (string instanceof Uint8Array) {
+		const xmlDetection = isXml(string);
+		if (xmlDetection) {
+			const textDecoder = new TextDecoder(xmlDetection.encoding);
+			return isSvg(textDecoder.decode(string));
+		}
 		return false;
 	}
 
-	const xmlTextDetector = new XmlTextDetector();
-	xmlTextDetector.write(string);
-	return xmlTextDetector.isValid() && xmlTextDetector.fileType?.ext === 'svg';
+	throw new TypeError(`Expected a \`string\` or \`Uint8Array\`, got \`${typeof string}\``);
 }
